d41421161c0588d460925959af4fd728449b27c1
ilan-stern <ilan.stern@me.com>
2024-10-02 21:28:21 +1000

    Foundations for iOS

---
+++ my-ios-app/database/config.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+    user: 'your-username',
+    host: 'localhost',
+    database: 'mydb',
+    password: 'password',
+    port: 5432,
+});
+
+module.exports = pool;
